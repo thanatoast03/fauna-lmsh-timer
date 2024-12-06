5ccf4e2e2d878b5d3ae20949979ca99133611630
Jack C <jkchin2@buffalo.edu>
2024-12-05 21:24:27 -0500

    setup page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';  // Import the Tailwind CSS file
+import LandingPage from "./landing.jsx";
+
+function App() {
+    return(
+        <LandingPage></LandingPage>
+    )
+}
+
+export default App;
---
+++ src/landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="flex justify-center items-center text-center h-screen">
+            <h1 className='text-4xl'>Let Me Stay Here's Since Epoch</h1>
+        </div>
+    )
+}
+
+export default Landing;
